3dd425b8c6a514bbac0dc88679aa732557fdca33
PhongND <duyphong05012000@gmail.com>
2022-01-05 20:30:06 +0700

    done 93

--- ChuongTrinh/ChuongTrinh/Areas/Admin/Controllers/ProductController.cs
+++ ChuongTrinh/ChuongTrinh/Areas/Admin/Controllers/ProductController.cs
@@ -17,12 +17,68 @@ namespace ChuongTrinh.Areas.Admin.Controllers
         private QuanLyBanHangDB db = new QuanLyBanHangDB();
 
         // GET: Admin/Product
-        public ActionResult Index()
+        public ActionResult Index(string searchString, string sortOrder, string currentFilter, int? page, int? size)
         {
-            var sanPhams = db.SanPhams.Include(s => s.DanhMuc);
-            return View(sanPhams.ToList());
+            /*if (Session["Username"] != null)
+            {*/
+            ViewBag.CurrentSort = sortOrder; // Lấy biến yêu cầu sắp xếp hiện tại
+            ViewBag.SapTheoTen = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.SapTheoGiaBan = sortOrder == "GiaBan" ? "giaban_desc" : "GiaBan";
+
+            if (searchString != null)
+            {
+                page = 1; //trang đầu tiên
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var sanPhams = db.SanPhams.Include(p => p.DanhMuc);
+            //Lọc theo tên
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sanPhams = sanPhams.Where(p => p.TenSP.Contains(searchString));
+            }
+            //Sắp xếp
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.TenSP);
+                    break;
+                case "giaban_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.GiaBan);
+                    break;
+                default:
+                    sanPhams = sanPhams.OrderBy(s => s.TenSP);
+                    break;
+            }
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "10", Value = "10" });
+            items.Add(new SelectListItem { Text = "20", Value = "20" });
+            items.Add(new SelectListItem { Text = "50", Value = "50" });
+
+            foreach (var item in items)
+            {
+                if (item.Value == size.ToString()) item.Selected = true;
+            }
+
+            ViewBag.size = items; // ViewBag DropDownList
+            ViewBag.currentSize = size; // tạo biến kích thước trang hiện tại
+
+            int pageSize = (size ?? 10);
+            int pageNumber = (page ?? 1);
+
+            return View(sanPhams.ToPagedList(pageNumber, pageSize));
+            /*   }
+               else
+               {
+                   return RedirectToAction("Index", "Account");
+               }*/
         }
-        
+
         // GET: Admin/Product/Details/5
         public ActionResult Details(int? id)
         {
@@ -51,25 +107,31 @@ namespace ChuongTrinh.Areas.Admin.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "MaSP,TenSP,TrangThai,KhoiLuong,GiaBan,MoTa,HinhAnh,MaDanhMuc")] SanPham sanPham,
-           HttpPostedFileBase file )
+           HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+            try
             {
-                //Kiểm tra nếu có ảnh thì lưu ảnh vào server và tên ảnh vào database
-                if (file != null && file.ContentLength>0 )
+                if (ModelState.IsValid)
                 {
-                    string fileName = Path.GetFileName(file.FileName);
-                    string pathServer = Path.Combine(Server.MapPath("~/wwwroot/Client/images/products"), fileName);
-                    file.SaveAs(pathServer);
-                    sanPham.HinhAnh = fileName;
-                }    
-                db.SanPhams.Add(sanPham);
-                db.SaveChanges();
+                    //Kiểm tra nếu có ảnh thì lưu ảnh vào server và tên ảnh vào database
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string pathServer = Path.Combine(Server.MapPath("~/wwwroot/Client/images/products"), fileName);
+                        file.SaveAs(pathServer);
+                        sanPham.HinhAnh = fileName;
+                    }
+                    db.SanPhams.Add(sanPham);
+                    db.SaveChanges();
+                }
                 return RedirectToAction("Index");
             }
-
-            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
-            return View(sanPham);
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu!" + ex.Message;
+                ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
+                return View(sanPham);
+            }
         }
 
         // GET: Admin/Product/Edit/5
@@ -96,24 +158,31 @@ namespace ChuongTrinh.Areas.Admin.Controllers
         public ActionResult Edit([Bind(Include = "MaSP,TenSP,TrangThai,KhoiLuong,GiaBan,MoTa,HinhAnh,MaDanhMuc")] SanPham sanPham,
             HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+            try
             {
-                // Kiem tra neu co anh thi luu anh vao server va ten anh vao database
-                if (file != null && file.ContentLength > 0)
+                if (ModelState.IsValid)
                 {
-                    string fileName = Path.GetFileName(file.FileName);
-                    string pathServer = Path.Combine(Server.MapPath("/wwwroot/Client/images/products"), fileName);
+                    // Kiem tra neu co anh thi luu anh vao server va ten anh vao database
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string pathServer = Path.Combine(Server.MapPath("/wwwroot/Client/images/products"), fileName);
 
-                    file.SaveAs(pathServer);
-                    sanPham.HinhAnh = fileName;
+                        file.SaveAs(pathServer);
+                        sanPham.HinhAnh = fileName;
 
+                    }
+                    db.Entry(sanPham).State = EntityState.Modified;
+                    db.SaveChanges();
                 }
-                db.Entry(sanPham).State = EntityState.Modified;
-                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
-            return View(sanPham);
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu!" + ex.Message;
+                ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", sanPham.MaDanhMuc);
+                return View(sanPham);
+            }
         }
 
         // GET: Admin/Product/Delete/5
@@ -137,17 +206,25 @@ namespace ChuongTrinh.Areas.Admin.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             SanPham sanPham = db.SanPhams.Find(id);
-            //lấy danh sách hóa đơn 
-            ICollection<ChiTietHoaDon> chiTietHoaDons = sanPham.ChiTietHoaDons;
-            //Kiểm tra nếu sản phẩm tồn tại trong hóa đơn thì thông báo lỗi không thể xóa
-            if (chiTietHoaDons != null && chiTietHoaDons.Count > 0)
-            {
-                ModelState.AddModelError("messages", "Sản phẩm này tồn tại trong hóa đơn, không thể xóa");
-                return View(sanPham);
+            try
+            {   //lấy danh sách hóa đơn 
+                ICollection<ChiTietHoaDon> chiTietHoaDons = sanPham.ChiTietHoaDons;
+                //Kiểm tra nếu sản phẩm tồn tại trong hóa đơn thì thông báo lỗi không thể xóa
+                if (chiTietHoaDons != null && chiTietHoaDons.Count > 0)
+                {
+                    ModelState.AddModelError("messages", "Sản phẩm này tồn tại trong hóa đơn, không thể xóa");
+                    return View(sanPham);
+                }
+                db.SanPhams.Remove(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
-            db.SanPhams.Remove(sanPham);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Không xoá được bản ghi này !" + ex.Message;
+                return View("Delete", sanPham);
+            }
+
         }
 
         protected override void Dispose(bool disposing)
--- ChuongTrinh/ChuongTrinh/Models/SanPham.cs
+++ ChuongTrinh/ChuongTrinh/Models/SanPham.cs
@@ -5,6 +5,7 @@
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.ComponentModel;
 
     [Table("SanPham")]
     public partial class SanPham
@@ -19,12 +20,12 @@
         [Display(Name = "Mã SP")]
 
         public int MaSP { get; set; }
-        [Display(Name = "Tên SP")]
-        [Required(ErrorMessage ="Tên SP không được để trống !")]
+        [Display(Name = "Tên sản phẩm")]
+        [Required(ErrorMessage = "Tên sản phẩm không được để trống !")]
         [StringLength(255)]
         public string TenSP { get; set; }
         [Display(Name = "Trạng thái")]
-        
+
         public bool? TrangThai { get; set; }
         [Display(Name = "Khối lượng")]
         [Required(ErrorMessage = "Khối lượng không được để trống !")]
@@ -32,8 +33,10 @@
         [Display(Name = "Giá bán")]
 
         [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:#,###}" + " VNĐ")]
         public decimal GiaBan { get; set; }
         [Display(Name = "Mô tả")]
+        [DataType(DataType.MultilineText)]
 
         [Column(TypeName = "ntext")]
         public string MoTa { get; set; }
@@ -41,9 +44,11 @@
 
         [StringLength(100)]
         public string HinhAnh { get; set; }
-        [Display(Name = "Danh mục")]
+        [DisplayName("Danh mục")]
+
 
         public int? MaDanhMuc { get; set; }
+        [Display(Name = "Danh mục")]
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; }
